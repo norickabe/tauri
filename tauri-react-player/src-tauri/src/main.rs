2c78af0804bb64cff201ff92f1c4a4c68c5188a0
norickabe <b1749777@planet.kanazawa-it.ac.jp>
2022-09-28 20:51:35 +0900

    create repository

---
+++ tauri-react-player/src-tauri/src/main.rs
@@ -0,0 +1,50 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+#[macro_use]
+extern crate serde;
+
+use std::fs;
+
+#[derive(Serialize)]
+#[serde(tag = "type")]
+enum Entry {
+    #[serde(rename = "file")]
+    File { name: String, path: String },
+    #[serde(rename = "dir")]
+    Dir { name: String, path: String },
+}
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn get_entries(path: &str) -> Result<Vec<Entry>, String> {
+    let entries = fs::read_dir(path).map_err(|e| format!("{}", e))?;
+
+    let res = entries
+        .filter_map(|entry| -> Option<Entry> {
+            let entry = entry.ok()?;
+            let name = entry.file_name().to_string_lossy().to_string();
+            let path = entry.path().to_string_lossy().to_string();
+            let type_ = entry.file_type().ok()?;
+
+            if type_.is_dir() {
+                Some(Entry::Dir { name, path })
+            } else if type_.is_file() {
+                Some(Entry::File {name, path })
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    Ok(res)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_entries])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ tauri-react-player/src/App.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
+import { homeDir } from '@tauri-apps/api/path';
+import { convertFileSrc } from '@tauri-apps/api/tauri';
+import { invoke } from '@tauri-apps/api';
+
+type Entry = {
+  type: 'dir' | 'file';
+  name: string;
+  path: string;
+};
+
+type Entries = Array<Entry>;
+
+const App = () => {
+  const [src, setSrc] = useState<string | null>(null);
+  const [dir, setDir] = useState<string | null>(null);
+  const [player, setPlayer] = useState<JSX.Element | null>(null);
+  const [entries, setEntries] = useState<Entries | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const home = await homeDir();
+      setDir(home);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (!src) {
+        return;
+      }
+      
+      const url = convertFileSrc(src);
+      const player = <ReactPlayer url={url} controls={true} />
+      setPlayer(player);
+    })();
+  }, [src]);
+
+  useEffect(() => {
+    (async () => {
+      const entries = await invoke<Entries>("get_entries", { path: dir })
+        .catch(err => {
+          console.error(err);
+          return null;
+        });
+
+      setEntries(entries);
+    })();
+  }, [dir]);
+
+  const entry_list = entries ? <ul>
+    {entries.map(entry => {
+      if (entry.type === "dir") {
+        return <li key={entry.path} onClick={() => setDir(entry.path)}>{entry.name}</li>;
+      } else {
+        return <li key={entry.path} onClick={() => setSrc(entry.path)}>{entry.name}</li>;
+      }
+    })}
+    </ul> : null;
+
+  return (
+    <>
+      <h1>React Player</h1>
+      {player}
+      <br />
+      src: {src ?? '(not selected)'}
+      <br />
+      dir: {dir ?? ''}
+      <br />
+      {entry_list}
+      <br />
+    </>
+  );
+}
+
+export default App;
